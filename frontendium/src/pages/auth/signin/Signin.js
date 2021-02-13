16f804d503f3728d6d666559fb6edcf47ce0a4f5
Calvin Ly <lycalvin99@gmail.com>
2021-02-12 16:55:23 -0800

    Refactor landing page

--- frontendium/src/pages/auth/signin/Signin.js
+++ frontendium/src/pages/auth/signin/Signin.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+import Lottie from 'react-lottie';
 import { Link } from "react-router-dom";
-import { Row, Button, InputGroup, Form, FormControl } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
+import { Row, InputGroup, FormControl } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import UFOData from '../../../images/ufo';
 import './Signin.css';
 
 function Signin() {
+
+    const UFO = {
+        loop: true,
+        autoplay: true,
+        animationData: UFOData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
 
     return (
         <div id="Login">
@@ -42,6 +53,13 @@ function Signin() {
                 <Row className="bottom-login-form-row">
                     <p>Forgot your password? <Link to="forgotpassword">Reset password</Link></p>
                 </Row>
+            </div>
+            <div className="auth-lottie-img">
+                <Lottie
+                    options={UFO}
+                    height={300}
+                    width={300}
+                />
             </div>
         </div>
     );
