130435672af09325df3315489d7f0b9d3e24a306
Calvin Ly <lycalvin99@gmail.com>
2021-03-15 20:22:41 -0700

    Finished Indeed Job Scraping

--- backendium/webscrape/Indeed.js
+++ backendium/webscrape/Indeed.js
@@ -4,6 +4,7 @@ const indeed = require('indeed-scraper');
 
 async function indeedScraper(jobInfo) {
     let indeedJobs = [];
+    let filteredJobs = [];
     let keywords = jobInfo.keywords.replace(/\s/g, '+');
     let location = jobInfo.location.replace(/\s/g, '+');
     let indeedLink = "https://www.indeed.com/jobs?q=" + keywords + "&l=" + location;
@@ -12,33 +13,58 @@ async function indeedScraper(jobInfo) {
     puppeteer.launch({ headless: true })
         .then(async (browser) => {
             let page = await browser.newPage();
-            // Visit Up To Page 8 Of Indeed Job Search
-            for (var i = 1; i < 9; i++) {
-                let currentIndeedLink = indeedLink + "&start=" + i * 10;
+            // Visit Up To Page N Of Indeed Job Search
+            for (var N = 1; N < 2; N++) {
+                let currentIndeedLink = indeedLink + "&start=" + N * 10;
                 // Load Indeed Job Search Page
                 await page.goto(currentIndeedLink, { waitUntil: 'domcontentloaded' })
-                    .then(() => {
+                    .then(async () => {
                         const content = page.content();
-                        content.then((success) => {
+                        await content.then((success) => {
                             const $ = cheerio.load(success);
                             // Retrieve The Content For Each Job
                             $('div.jobsearch-SerpJobCard').each(function (i, element) {
-                                indeedJobs.push({
-                                    title: $(this).find('.jobtitle').text().trim(),
-                                    company: $(this).find('.company').text().trim(),
-                                    location: $(this).find('.location').text().trim(),
-                                    link: "https://www.indeed.com" + $(this).find('.title a').attr('href'),
-                                    description: ""
-                                });
+                                let indeedRedirectLink = $(this).find('.title a').attr('href');
+                                // Filter Some Of The Jobs That Are Not External Redirects
+                                if (indeedRedirectLink.slice(0, 10) === "/rc/clk?jk") {
+                                    indeedJobs.push({
+                                        title: $(this).find('.jobtitle').text().trim(),
+                                        company: $(this).find('.company').text().trim(),
+                                        location: $(this).find('.location').text().trim(),
+                                        link: "https://www.indeed.com" + $(this).find('.title a').attr('href')
+                                    });
+                                }
                             });
                         })
                     })
             }
-
-            // TODO: Filter Job Listings With An "External" Apply Link
-            //
-            // TODO: For 'indeedJobs' variable, replace 'link' and 'description'
-            //
+            // Loop Through Each Saved Job To Retrieve External Job Link And Job Description
+            for (var i = 0; i < indeedJobs.length; i++) {
+                const job = indeedJobs[i];
+                // Load Indeed Job Information Page
+                await page.goto(job.link, { waitUntil: 'domcontentloaded' })
+                    .then(async () => {
+                        const content = page.content();
+                        await content.then((success) => {
+                            const $ = cheerio.load(success);
+                            // Retrieve External Job Link and Job Description
+                            let externalJobLink = $('#applyButtonLinkContainer').find('a').attr('href');
+                            if (externalJobLink !== undefined) {
+                                filteredJobs.push({
+                                    title: indeedJobs[i].title,
+                                    company: indeedJobs[i].company,
+                                    location: indeedJobs[i].location,
+                                    link: externalJobLink,
+                                    description: $('#jobDescriptionText').html()
+                                });
+                            }
+                        })
+                    })
+                    .catch((err) => {
+                        console.log("Error: ", error);
+                    })
+            }
+            return filteredJobs;
         })
         .catch(err => {
             console.log("Error: ", err);
