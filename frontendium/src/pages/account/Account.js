026839467a27b995368e21db8cc5960511b40d16
Davin Wong <davinlamwong@gmail.com>
2021-03-27 23:14:19 -0700

    Added more to account page but looks doomed save me calvin

--- frontendium/src/pages/account/Account.js
+++ frontendium/src/pages/account/Account.js
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
-import Lottie from 'react-lottie';
-import { Link } from "react-router-dom";
-import { Row, Col, InputGroup, FormControl } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from 'react';
+import ReactRoundedImage from 'react-rounded-image';
+import { Button } from 'react-bootstrap';
+import { currentEmail } from '../../api/Auth';
+import { UserInfo } from '../../api/UserInfo';
 import './Account.css';
+import TempPfp from '../../images/Astronaut.png';
+
+async function getUserInfo() {
+    const userEmail = currentEmail();
+    const userInfo = await UserInfo({ email: userEmail });
+    console.log('check' + userInfo);
+}
 
 function Account() {
+    const [info, setInfo] = useState();
+    getUserInfo();
+
     return (
         <div id="Account">
             <div id="prof-pic-and-name">
-                <span>
-                    hi
-                </span>
+                <ReactRoundedImage 
+                    image={TempPfp}
+                    roundedColor="white"
+                    imageWidth="120"
+                    imageHeight="120"
+                    roundedSize="8"
+                />
+                <h1 className="username">hi</h1>
             </div>
             <hr className="lines" />
             <div id="profile">
                 <h2>Profile</h2>
+                <div id="profile-content">
+                    <h5>Name:</h5>
+                    <h5>Email:</h5>
+                    <h5>Major:</h5>
+                </div>
+                <Button id="edit-profile-btn">Edit Profile</Button>
             </div>
             <hr className="lines" />
             <div id="jobs-applied-to">
