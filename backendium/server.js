7a56250228eadef32600b0b88de7dd0a95bcc615
wilsonzhang6 <wilsonzhang6@gmail.com>
2021-03-29 14:35:57 -0700

    Add AWS SDK

--- backendium/server.js
+++ backendium/server.js
@@ -46,6 +46,60 @@ var gracefulExit = function () {
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
 
+// Verify credentials were passed in correctly
+var AWS = require("aws-sdk");
+AWS.config.getCredentials(function (err) {
+  if (err) console.log(err.stack);
+  else {
+    console.log("Access key", AWS.config.credentials.accessKeyId);
+  }
+})
+
+// Import required AWS SDK clients and commands for Node.js
+const {
+  S3Client,
+  PutObjectCommand,
+  CreateBucketCommand,
+  ListBucketsCommand
+} = require("@aws-sdk/client-s3");
+
+// Set the AWS region
+const REGION = "us-west-1"; // e.g., "us-east-1"
+
+// Set the bucket parameters
+const bucketName = "cmpendium";
+const bucketParams = { Bucket: bucketName };
+
+// Create an S3 client service object
+const s3 = new S3Client({
+  accessKeyId: "AKIAYYABHORWBZ5UPQ5F",
+  secretAccessKey: "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",
+  region: REGION
+});
+
+const listBuckets = async () => {
+  try {
+    const data = await s3.send(new ListBucketsCommand({}));
+    console.log("Success", data.Buckets);
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+listBuckets();
+
+// Create name for uploaded object key
+const keyName = "hello_world.txt";
+const objectParams = { Bucket: bucketName, Key: keyName, Body: "Hello World!" };
+
+const upload = async () => {
+  try {
+    const results = await s3.send(new PutObjectCommand(objectParams));
+    console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
 const port = 9000;
 app.listen(port, function () {
   console.log('Express server is running on port 9000')
